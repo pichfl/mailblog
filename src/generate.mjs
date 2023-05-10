fe632fcc00cf80cba25f20e36c733de6b4071b33
Florian Pichler <fp@ylk.gd>
2023-05-10 17:26:38 +0200

    Allow file removal

--- src/generate.mjs
+++ src/generate.mjs
@@ -3,6 +3,7 @@ import { readdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { deleteAsync } from 'del';
 import { glob } from 'glob';
+import { config } from './config.mjs';
 
 // Read `./in` directory and transform all mails
 
@@ -12,7 +13,10 @@ export default async function (inDir, outDir) {
   await Promise.all(
     files.map(async (file) => {
       const result = await transformMail(join(inDir, file), outDir);
-      // await deleteAsync(join(inDir, file));
+
+      if (config.removeFiles) {
+        await deleteAsync(join(inDir, file));
+      }
 
       return result;
     })
