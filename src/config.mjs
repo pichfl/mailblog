6cb7353f28b47f2c143c7a6487fc2fdfafef904d
Florian Pichler <fp@ylk.gd>
2023-05-10 19:21:15 +0200

    Add deploy hook

--- src/config.mjs
+++ src/config.mjs
@@ -6,4 +6,5 @@ export const config = {
   inDirectory: process.env.IN_DIRECTORY ?? './in',
   outDirectory: process.env.OUT_DIRECTORY ?? './out',
   removeFiles: process.env.REMOVE_FILES === 'true' ?? false,
+  deployHook: process.env.DEPLOY_HOOK ?? '',
 };
--- src/generate.mjs
+++ src/generate.mjs
@@ -4,6 +4,7 @@ import { join } from 'node:path';
 import { deleteAsync } from 'del';
 import { glob } from 'glob';
 import { config } from './config.mjs';
+import got from 'got';
 
 // Read `./in` directory and transform all mails
 
@@ -25,7 +26,13 @@ export default async function (inDir, outDir) {
 
   const matches = await glob('**/*.json', { cwd: outDir });
 
-  writeFile(join(outDir, `index.json`), JSON.stringify(matches), {
+  await writeFile(join(outDir, `index.json`), JSON.stringify(matches), {
     encoding: 'utf-8',
   });
+
+  if (config.deployHook) {
+    await got(config.deployHook, {
+      method: 'POST',
+    });
+  }
 }
