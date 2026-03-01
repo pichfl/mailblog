d386545caa77d698b6d162a074cbe9f54d689ef5
Florian Pichler <pichfl@ylk.gd>
2026-03-01 17:14:48 +0100

    Drop options for writing out index file

--- src/main.js
+++ src/main.js
@@ -1,52 +1,33 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import got from 'got';
+import { createReadStream } from 'node:fs';
 
 import { config } from './config.js';
 import convertMail from './convert-mail.js';
 import writeContent from './write/content.js';
-import writeIndex from './write/index.js';
 
 const program = new Command();
 
 program
 	.name('mailmd')
-	.description('Transform piped .eml files into markdown and images')
+	.description('Transform .eml files into markdown and images')
 	.version(process.env.npm_package_version);
 
 program
-	.option('-o, --out <outDir>', 'Output directory')
-	.option('-i, --index', 'Write index.json', false)
-	.option('-c, --content', 'Write content.json', false)
-	.option('-r, --read', 'Read Email and write post.md', false)
-	.option('-p, --ping <ping>', 'Ping HTTP trigger', '');
+	.argument('[file]', 'Email file to convert (uses stdin if not provided)')
+	.option('-o, --out <outDir>', 'Output directory');
 
 program.parse(process.argv);
 
 const options = program.opts();
+const args = program.args;
 const outDir = options.out ?? config.outDirectory;
-const ping = options.ping || config.deployHook;
 
 try {
-	if (options.read) {
-		const written = await convertMail(process.stdin, outDir);
-
-		console.log(written);
-	}
-
-	if (options.content) {
-		await writeContent(outDir);
-	}
-
-	if (options.index) {
-		await writeIndex(outDir);
-	}
-
-	if (ping) {
-		await got(ping, {
-			method: 'POST',
-		});
-	}
+	const inputStream = args[0] ? createReadStream(args[0]) : process.stdin;
+  const written = await convertMail(inputStream, outDir);
+  await writeContent(outDir);
+	console.log(written);
 } catch (error) {
 	console.error(error.message);
 	if (error.cause) console.error(error.cause);
