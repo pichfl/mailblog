4f881cfb9237f34b4a16d1706e13de6f327dfecc
Florian Pichler <pichfl@ylk.gd>
2026-03-14 00:06:40 +0100

    Don't log salt

--- packages/dog/src/index.js
+++ packages/dog/src/index.js
@@ -10,9 +10,9 @@ program
 	.argument('[in]', 'inbox directory to watch', process.env.POSTEINGANG_IN ?? './in')
 	.argument('[dist]', 'dist directory for output', process.env.POSTEINGANG_OUT ?? './dist')
 	.option('-d, --deploy-hook <url>', 'webhook URL to POST to after rebuild')
-	.option('-s, --salt <value>', 'HMAC-SHA256 salt to verify sender hash in recipient address', process.env.POSTEINGANG_SALT)
+	.option('-s, --salt <value>', 'HMAC-SHA256 salt to verify sender hash in recipient address')
 	.action((inDir, distDir, { deployHook, salt }) => {
-		createWatcher(inDir, distDir, { deployHook, salt  });
+		createWatcher(inDir, distDir, { deployHook, salt: salt || process.env.POSTEINGANG_SALT  });
 		console.log(`@posteingang/dog watching ${inDir}`);
 	});
 
